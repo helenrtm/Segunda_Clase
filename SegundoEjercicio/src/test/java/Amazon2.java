36399b7f6d743325f7c15b36f45344527cac80dc
helenrtm <helenrtm2003@gmail.com>
2020-12-20 00:32:58 -0500

    Segundo ejercicio

--- SegundoEjercicio/src/test/java/Amazon2.java
+++ SegundoEjercicio/src/test/java/Amazon2.java
@@ -45,6 +45,44 @@ public class Amazon2 {
             e.printStackTrace();
         }
 
+
+        String numuno = "//*[@id=\'search\']/div[1]/div[2]/div/span[3]/div[2]/div[3]/div/span/div/div/div/div/span/a/div/img";
+
+        WebElement numsel = driver.findElement(By.xpath(numuno));
+        numsel.click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+        //--
+
+        String localizadorXpath1 = "//*[@id=\'twotabsearchtextbox\']";
+
+        WebElement inputBusqueda1 = null;
+
+        //driver al colocarle un xpath se convierte en un webelemnt
+        inputBusqueda1 = driver.findElement(By.xpath(localizadorXpath1));
+        inputBusqueda1.clear();
+        inputBusqueda1.sendKeys("zapatillas");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        inputBusqueda1.sendKeys(Keys.ENTER);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
         String detalleProd1 = driver.findElement(By.xpath("//*[@id=\'search\']/div[1]/div[2]/div/span[3]/div[2]/div[3]/div/span/div/div/div/div/div[2]/h2/a/span")).getText();
 
         System.out.println("Detalle de la zapatilla #1: "+  (detalleProd1));
@@ -60,15 +98,16 @@ public class Amazon2 {
         double p1 = Double.parseDouble(precioProd1);
         double p2 = Double.parseDouble(precioProd2);
 
-        if (p1==p2){
-            System.out.println ("Los precios son iguales, puede llevar el de su preferencia, precio: " + precioProd1);
-            if (p1>p2){
-                System.out.println ("La mejor opción de compra el el producto # 2, precio: " + precioProd2);
-            }
+        if (p1==p2) {
+            System.out.println("Los precios son iguales, puede llevar el de su preferencia, precio: " + precioProd1);
         }
+            if (p1>p2) {
+                System.out.println("La mejor opción de compra el el producto # 2, precio: " + precioProd2);
+            }
+                if (p1<p2) {
+                    System.out.println("La mejor opción de compra el el producto # 1, precio: " + precioProd1);
 
-        System.out.println ("La mejor opción de compra el el producto # 2, precio: " + precioProd2);
-
+                }
 
 
         try {
